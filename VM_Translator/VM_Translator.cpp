f6c5fa3e9b3db1d2a0bd46898f9985e444eb3f47
kblee05 <kennethbwlee@snu.ac.kr>
2025-12-12 18:31:36 +0900

    added arithmetic and logical commands

--- VM_Translator/VM_Translator.cpp
+++ VM_Translator/VM_Translator.cpp
@@ -10,7 +10,14 @@ std::map<std::string, std::string> memory_segment = {
     {"that", "THAT"}
 };
 
-void write_push_pop(std::ofstream& output_file, std::string operation, std::string segment_type, int index);
+std::map<std::string, std::string> jump = {
+    {"eq", "JEQ"},
+    {"gt", "JGT"},
+    {"lt", "JLT"}  
+};
+
+void write_push_pop(std::ofstream& output_file, std::string command, std::string segment_type, int index);
+void write_stack_commands(std::ofstream& output_file, std::string command);
 
 int main(){
 
@@ -30,17 +37,26 @@ int main(){
 
             write_push_pop(output_file, input, segment_type, index);
         }
+        else if(input == "add" || input == "sub" || input == "neg" ||
+                input == "eq"  || input == "gt"  || input == "lt"  ||
+                input == "and" || input == "or"  || input == "not")
+        {
+            write_stack_commands(output_file, input);
+        }
     }
+
+    input_file.close();
+    output_file.close();
 
     return 0;
 }
 
-void write_push_pop(std::ofstream& output_file, std::string operation, std::string segment_type, int index){
+void write_push_pop(std::ofstream& output_file, std::string command, std::string segment_type, int index){
 
     // commment
-    output_file << "// " << operation << " " << segment_type << " " << index << "\n";
+    output_file << "// " << command << " " << segment_type << " " << index << "\n";
 
-    if(operation == "push"){
+    if(command == "push"){
 
         if(segment_type == "constant"){
             output_file << "@" << index << "\n";
@@ -69,7 +85,7 @@ void write_push_pop(std::ofstream& output_file, std::string operation, std::stri
         output_file << "@SP\n";
         output_file << "M=M+1\n";
     }
-    else if(operation == "pop"){
+    else if(command == "pop"){
         // Store memory segment address
         output_file << "@" << memory_segment[segment_type] << "\n";
         output_file << "D=M\n";
@@ -86,5 +102,74 @@ void write_push_pop(std::ofstream& output_file, std::string operation, std::stri
         output_file << "@R13\n";
         output_file << "A=M\n";
         output_file << "M=D\n";
+    }
+}
+
+void write_stack_commands(std::ofstream& output_file, std::string command){
+    // comment
+    output_file << "// " << command << "\n";
+
+    if(command == "neg" || command == "not"){ // Need one item from stack
+        output_file << "@SP\n";
+        output_file << "AM=M-1\n";
+        
+        output_file << (command == "neg" ? "M=-M\n" : "M=!M\n");
+
+        output_file << "@SP\n";
+        output_file << "M=M+1\n";
+    }
+    else{ // Need two items from Stack
+        // Item A
+        output_file << "@SP\n";
+        output_file << "AM=M-1\n";
+        output_file << "D=M\n";
+        output_file << "@R13\n";
+        output_file << "M=D\n";
+
+        // Item B
+        output_file << "@SP\n";
+        output_file << "AM=M-1\n";
+        output_file << "D=M\n";
+
+        // Restore Item A
+        output_file << "@R13\n";
+
+        if(command == "add")
+            output_file << "D=D+M\n";
+        else if(command == "sub")
+            output_file << "D=D-M\n"; // Item B - Item A (B was pushed earlier than A)
+        else if(command == "and")
+            output_file << "D=D&M\n";
+        else if(command == "or")
+            output_file << "D=D|M\n";
+        else{ // eq, gt, lt
+            static int label_count = 0;
+
+            // if statement
+            output_file << "D=D-M\n"; // Item B - Item A (B was pushed earlier than A)
+            output_file << "@TRUE_" << label_count << "\n";
+            output_file << "D;" << jump[command] << "\n";
+            
+            // false
+            output_file << "D=0\n";
+            output_file << "@END_" << label_count << "\n";
+            output_file << "0;JMP\n";
+
+            // true
+            output_file << "(TRUE_" << label_count << ")\n";
+            output_file << "D=-1\n";
+            
+            // end
+            output_file << "(END_" << label_count << ")\n";
+
+            label_count++;
+        }
+
+        // push final result
+        output_file << "@SP\n";
+        output_file << "A=M\n";
+        output_file << "M=D\n";
+        output_file << "@SP\n";
+        output_file << "M=M+1\n";
     }
 }
